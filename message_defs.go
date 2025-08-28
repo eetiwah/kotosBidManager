68a455625129a2fe5b565cd5ef6ef4505a527c5a
eetiwah <rweir@jibberish-networks.com>
2025-08-28 17:30:58 -0500

    BidObject definition was out of sync with the community

--- message_defs.go
+++ message_defs.go
@@ -12,24 +12,20 @@ type AuctionObject struct {
 	WinningBid string    `json:"winningbid"` // Winning bid of an auction
 }
 
-type Response struct {
-	GUID    string   `json:"guid"`
-	Updated int      `json:"updated"`
-	Errors  []string `json:"errors,omitempty"`
-}
-
 type BidObject struct {
-	BidId        string    `json:"bidid"`        // GUID for the bid
-	AuctionId    string    `json:"auctionid"`    // GUID for an auction
-	Price        string    `json:"price"`        // Target price
-	Quantity     int       `json:"quantity"`     // Quantity requested
-	DeliveryDate time.Time `json:"deliverydate"` // Delivery date
-	Onion        string    `json:"onion"`        // Onion address of the bid submitter
-	ResponseDate time.Time `json:"responsedate"` // The time the bid was submitted
+	BidId     string `json:"bidid"`     // GUID for the bid
+	AuctionId string `json:"auctionid"` // GUID for an auction
+	Price     string `json:"price"`     // Target price
+	// Quantity     int       `json:"quantity"`     // Quantity requested
+	// DeliveryDate time.Time `json:"deliverydate"` // Delivery date
+	// Onion        string    `json:"onion"`        // Onion address of the bid submitter
+	// ResponseDate time.Time `json:"responsedate"` // The time the bid was submitted
 }
 
+/*
 type BidResponse struct {
 	AuctionID    string    `json:"auctionid"`    // GUID for the auction
 	BidId        string    `json:"bidid"`        // GUID for the bid
 	ResponseDate time.Time `json:"responsedate"` // end date
 }
+*/
